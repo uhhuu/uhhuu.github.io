b2a97c63adb06e98cb3c915de1d2d5d23c2f9bba
Luria Urali <luriaurali@protonmail.com>
2023-01-11 10:35:21 +0200

    added tooltips and home button

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
 // google sheets table URL (the table must be published to Web in csv format)
 let gsheetURL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRgYjnAPdiJiOVcu6T1kCYIEnz27r_7It7Ipd1-G-hVXeVSHuGsU0NIl9QcB0y-qnXj_bY9ko8zfV5V/pub?output=csv"
 let map
+const boundsMinLat = 0
+const boundsMinLng = 0
+const boundsMaxLat = 3000
+const boundsMaxLng = 4000
+let homeLat = (boundsMaxLat - boundsMinLat) / 2
+let homeLng = (boundsMaxLng - boundsMinLng) / 2
+const defaultZoom = 0
 
 window.addEventListener("DOMContentLoaded", init);
 
@@ -9,16 +16,16 @@ window.addEventListener("DOMContentLoaded", init);
 */
 function init() {
     // Map initialization
-    const bounds = [L.latLng(0, 0), L.latLng(3000, 4000)];
+    const bounds = [L.latLng(boundsMinLat, boundsMinLng), L.latLng(boundsMaxLat, boundsMaxLng)];
 
     map = L.map('map', {
         crs: L.CRS.Simple,
         minZoom: -2,
         maxZoom: 2
     });
-    var image = L.imageOverlay('img/imogen_map.jpg', bounds).addTo(map);
+    let image = L.imageOverlay('img/imogen_map.jpg', bounds).addTo(map);
     map.fitBounds(bounds);
-    map.setZoom(-2);
+    map.setView([homeLat, homeLng],defaultZoom);
 
     // click to find coordinates
     // obtaining coordinates after clicking on the map
@@ -33,6 +40,66 @@ function init() {
         header: true,
         complete: parseSheet,
     });
+
+    // add Home button
+    const htmlTemplate =
+         '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"><path d="M32 18.451L16 6.031 0 18.451v-5.064L16 .967l16 12.42zM28 18v12h-8v-8h-8v8H4V18l12-9z" /></svg>';
+
+    // create custom button
+    const customControl = L.Control.extend({
+        // button position
+        options: {
+            position: "topleft",
+        },
+
+        // method
+        onAdd: function (map) {
+            // console.log(map.getCenter());
+            // create button
+            const btn = L.DomUtil.create("button");
+            btn.title = "back to home";
+            btn.innerHTML = htmlTemplate;
+            btn.className += "leaflet-bar back-to-home hidden";
+
+            return btn;
+        },
+    });
+
+    // adding new button to map controll
+    map.addControl(new customControl());
+
+    // on drag end
+    map.on("moveend", getCenterOfMap);
+
+    const buttonBackToHome = document.querySelector(".back-to-home");
+
+    function getCenterOfMap() {
+        buttonBackToHome.classList.remove("hidden");
+
+        buttonBackToHome.addEventListener("click", () => {
+            // to change zoom or not to change, that is the question
+            // map.flyTo([homeLat, homeLng], defaultZoom);
+            map.flyTo([homeLat, homeLng]);
+        });
+
+        map.on("moveend", () => {
+            const { lat: latCenter, lng: lngCenter } = map.getCenter();
+
+            const latC = latCenter.toFixed(3) * 1;
+            const lngC = lngCenter.toFixed(3) * 1;
+
+            const defaultCoordinate = [+homeLat.toFixed(3), +homeLng.toFixed(3)];
+
+            const centerCoordinate = [latC, lngC];
+
+            if (compareToArrays(centerCoordinate, defaultCoordinate)) {
+                buttonBackToHome.classList.add("hidden");
+            }
+        });
+    }
+
+    const compareToArrays = (a, b) => JSON.stringify(a) === JSON.stringify(b);
+
 }
 
 /*
@@ -48,7 +115,7 @@ function parseSheet(data) {
         let lng = parseFloat(data[row].lng);
         console.log(data[row]);
 
-        if (typeof (lat) == "number" && typeof (lng) == "number") {
+        if (lat > 0 && lng > 0) {
 
             // TODO: group markers to layers
             let layer = data[row].layer;
@@ -57,7 +124,8 @@ function parseSheet(data) {
             let iconName = data[row].icon
             let popupText = data[row].popupText
 
-            var marker = L.marker(L.latLng([lat, lng]));
+            let marker = L.marker(L.latLng([lat, lng]));
+            let hasMarker = false
 
             // use custom icon, if iconName is specified
             if (!(iconName === null || iconName.trim() === "") || !(popupText === null || popupText.trim() === "")) {
@@ -67,7 +135,7 @@ function parseSheet(data) {
                         // TODO: doesnt work, needs fixing
                         markerIcon = L.divIcon({
                             html: '<i class=' + iconName + '</i>',
-                            iconSize: [20, 20],
+                            iconSize: [30, 30],
                             className: 'myDivIcon'
                         });
                     } else {
@@ -76,17 +144,45 @@ function parseSheet(data) {
                             iconSize: [20, 20]
                         });
                     }
-                    console.log(markerIcon);
+                    // console.log(markerIcon);
                     marker.setIcon(markerIcon);
                 }
-                console.log(marker);
+                // console.log(marker);
                 marker.addTo(map).bindPopup(data[row].popupText);
+                hasMarker = true
             }
 
-            // TODO: if a mapText is defined, display text directly on map
+            // if a mapText is defined, display text directly on map using tooltip
+            const mapText = data[row].mapText
+            if (!(mapText === null || mapText.trim() === "")) {
+                let tooltipOptions = {
+                    permanent: true,
+                    offset: [0, 0],
+                    opacity: 0.5,
+                    className: 'my-tooltip'
+                }
+                // if marker is on map, add to marker, otherwise standalone
+                if (hasMarker) {
+                    marker.opacity = 0
+                    marker.bindTooltip(mapText, tooltipOptions);
+                    marker.addTo(map);
+                } else {
+                    var tooltip = L.tooltip(tooltipOptions).setLatLng([lat, lng]).setContent(mapText).addTo(map);
+                }
+                console.log(marker);
+            }
 
             // if the coordinates are set to show a home icon, set the home icon
+            isHome = data[row].home
+            if (!(isHome === null || isHome.trim() === "")) {
+                homeLng = lng
+                homeLat = lat
+            }
 
         }
     }
+
+    // fly to home position
+    map.flyTo([homeLat, homeLng]);
+
 }
