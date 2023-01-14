ba03d47f84f7275541863602417badf80a1aa221
Luria Urali <luriaurali@protonmail.com>
2023-01-15 00:28:45 +0200

    csv for image overlays, zoom-based opacity control

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 // Configuration - moved to index.html
 
 // global variables
-let map
-let homeLat = (boundsMaxLat - boundsMinLat) / 2
-let homeLng = (boundsMaxLng - boundsMinLng) / 2
-const defaultZoom = 0
+var map;
+var homeLat = -1, homeLng = -1;
+var isDoneParseCsvMapOverlays = true;
+var mapImageOverlays = [];
 
 window.addEventListener("DOMContentLoaded", init);
 
@@ -14,31 +14,20 @@ window.addEventListener("DOMContentLoaded", init);
 function init() {
     document.title = titleText
 
-    // Map initialization
-    const bounds = [L.latLng(boundsMinLat, boundsMinLng), L.latLng(boundsMaxLat, boundsMaxLng)];
-
+    // Base map initialization
     map = L.map('map', {
         crs: L.CRS.Simple,
-        minZoom: -2,
-        maxZoom: 5
+        minZoom: mapZoom[0],
+        maxZoom: mapZoom[1]
     });
-    map.attributionControl.addAttribution(baseMapAttribution);
-    let image = L.imageOverlay(baseMapURL, bounds).addTo(map);
-    map.fitBounds(bounds);
-    map.setView([homeLat, homeLng], defaultZoom);
+    // map.attributionControl.addAttribution(baseMapAttribution);
 
-/*    
-    // test an image overlay at specific pos
-    //    var bounds2 = [[1210, 2795], [1110, 2900.66]]
-    var bounds2 = [[1090, 1159], [1146, 1206]]
-    let image2 = L.imageOverlay("img/map_sample_city.png", bounds2, { opacity: 0 }).addTo(map);
-    map.on('zoomend', function () {
-        zoom = map.getZoom();
-        if (zoom < 0) image2.setOpacity(0);
-        else if (zoom >= 0 && zoom <= 2) image2.setOpacity((zoom+1)*0.2);
-        else image2.setOpacity(1);
-    });
-*/
+    let bounds = [[0, 0], mapBoundsMax]
+    let image = L.imageOverlay(baseMapUrl, bounds).addTo(map);
+    homeLat = mapBoundsMax[0] / 2
+    homeLng = mapBoundsMax[1] / 2
+    map.fitBounds(bounds);
+    map.setView([homeLat, homeLng], mapZoom[2]);
 
     // click to find coordinates
     // obtaining coordinates after clicking on the map
@@ -65,12 +54,12 @@ function init() {
         }
     });
 
-    // parse the csv table(s). Note that this runs asynchronously, don't expect it to have completed after this code block
+    // Parse the master csv table. Note that this runs asynchronously, don't expect it to have completed after this code block
     try {
-        Papa.parse(csvURL, {
+        Papa.parse(csvMasterUrl, {
             download: true,
             header: true,
-            complete: parseCsvMapMarkers,
+            complete: parseCsvMaster,
         });
     } catch (err) {
         console.log(err)
@@ -114,7 +103,7 @@ function init() {
 
         buttonBackToHome.addEventListener("click", () => {
             // to change zoom or not to change, that is the question
-            // map.flyTo([homeLat, homeLng], defaultZoom);
+            // map.flyTo([homeLat, homeLng], mapZoom[2]);
             map.flyTo([homeLat, homeLng]);
         });
 
@@ -128,48 +117,175 @@ function init() {
 
             const centerCoordinate = [latC, lngC];
 
-            if (compareToArrays(centerCoordinate, defaultCoordinate)) {
+            if (compareTwoArrays(centerCoordinate, defaultCoordinate)) {
                 buttonBackToHome.classList.add("hidden");
             }
         });
     }
 
-    const compareToArrays = (a, b) => JSON.stringify(a) === JSON.stringify(b);
+    // add handler for zoom change
+    map.on('zoomend', function () {
+        zoom = map.getZoom();
+        console.log("zoomend:", zoom)
+        for (let i = 0; i < mapImageOverlays.length; i++) {
+            for (let j = 0; j < mapImageOverlays[i].zoomOpacitys.length; j++) {
+                if (zoom <= mapImageOverlays[i].zoomOpacitys[j].minZoom) {
+                    mapImageOverlays[i].imageOverlay.setOpacity(mapImageOverlays[i].zoomOpacitys[j].opacity)
+                    break;
+                }
+            }
+
+        }
+        // if (zoom < 0) image2.setOpacity(0);
+        // else if (zoom >= 0 && zoom <= 2) image2.setOpacity((zoom+1)*0.2);
+        // else image2.setOpacity(1);
+    });
 
 }
 
+// compare arrays
+const compareTwoArrays = (a, b) => JSON.stringify(a) === JSON.stringify(b);
+
+
 /*
-    parseCsv() is the callback function to parse csv table data
+    parseCsvMaster() is the callback function to parse master csv table
 */
-function parseCsvMapMarkers(data) {
+function parseCsvMaster(data) {
     data = data.data;
 
     // for each row in table
     for (let row = 0; row < data.length; row++) {
-        console.log(data[row]);
+        console.log("csvMaster: ", data[row]);
+        // ignore comment lines (comment field starts with / or #)
+        if (data[row].comment[0] === "/" || data[row].comment[0] === "#") {
+            continue;
+        }
+
+        // change map attribution, if present
+        let mapAttribution = data[row].mapAttribution;
+        if (!(mapAttribution === null || mapAttribution === "")) {
+            map.attributionControl.addAttribution(mapAttribution);
+        }
+        // set page title, if present
+        let pageTitle = String(data[row].pageTitle).trim();
+        if (!(pageTitle === null || pageTitle === "")) {
+            document.title = pageTitle;
+        }
+
+        // parse map overlays csv, if present
+        let csvMapOverlaysUrl = String(data[row].csvMapOverlaysUrl).trim()
+        if (!(csvMapOverlaysUrl === null || csvMapOverlaysUrl === "")) {
+
+            // Parse the map markers csv table. Note that this runs asynchronously, don't expect it to have completed after this code block
+
+            // use a global variable to wait until this is done before moving on, otherwise some stuff (like flyto home marker) wont work
+            isDoneParseCsvMapOverlays = false;
+
+            try {
+                Papa.parse(csvMapOverlaysUrl, {
+                    download: true,
+                    header: true,
+                    complete: parseCsvMapOverlays,
+                });
+            } catch (err) {
+                console.log(err);
+                alert(err);
+            }
+
+            // TODO: put a timeout here
+            // while (!isDoneParseCsvMapOverlays) { }
+
+        }
+
+        // parse map markers csv, if present
+        let csvMapMarkersUrl = String(data[row].csvMapMarkersUrl).trim();
+        if (!(csvMapMarkersUrl === null || csvMapMarkersUrl === "")) {
+
+            // Parse the map markers csv table. Note that this runs asynchronously, don't expect it to have completed after this code block
+            try {
+                Papa.parse(csvMapMarkersUrl, {
+                    download: true,
+                    header: true,
+                    complete: parseCsvMapMarkers,
+                });
+            } catch (err) {
+                console.log(err);
+                alert(err);
+            }
+
+        }
+
+    }
+}
+
+/*
+    parseMapOverlays() is the callback function to parse master csv table
+*/
+function parseCsvMapOverlays(data) {
+    data = data.data;
+
+    // for each row in table
+    for (let row = 0; row < data.length; row++) {
+        console.log("csvMapOverlays: ", data[row]);
+        // ignore comment lines (comment field starts with / or #)
+        if (data[row].comment[0] === "/" || data[row].comment[0] === "#") {
+            continue;
+        }
+
+        let mapOverlayUrl = data[row].mapOverlayUrl;
+        if (!(mapOverlayUrl === null || mapOverlayUrl === "")) {
+            console.log("new imageOverlay: " + mapOverlayUrl);
+            let boundsMin = String(data[row].boundsMin).split(',');
+            let boundsMax = String(data[row].boundsMax).split(',');
+            let image = L.imageOverlay(mapOverlayUrl, [boundsMin, boundsMax]).addTo(map);
+            let mainMap = String(data[row].mainMap).trim().toUpperCase();
+            let overlayObject = {imageOverlay: image, zoomOpacitys: []}
+            let zoomOpacitys = String(data[row].zoomOpacitys).split(',');
+            console.log("zoomOpacitys",zoomOpacitys)
+            for (let i = 0; i < zoomOpacitys.length; i++) {
+                let zoomOpacity = String(zoomOpacitys[i]).split(':')
+                overlayObject.zoomOpacitys.push ({minZoom: zoomOpacity[0], opacity: zoomOpacity[1]});
+            }
+            mapImageOverlays.push(overlayObject)
+        }
+    }
+    isDoneParseCsvMapOverlays = true;
+}
+
+
+/*
+    parseCsvMapMarkers() is the callback function to parse map markers csv table data
+*/
+function parseCsvMapMarkers(data) {
+    data = data.data;
+    let newHome = false;
+
+    // for each row in table
+    for (let row = 0; row < data.length; row++) {
+        console.log("csvMapMarkers: ", data[row]);
 
         // ignore comment lines (comment field starts with / or #)
         if (data[row].comment[0] === "/" || data[row].comment[0] === "#") {
-            continue
+            continue;
         }
 
         // check that there are coordinates in the row
         let latlng = data[row].latlng;
         let commaPos = latlng.indexOf(',');
         if (latlng.indexOf(',') >= 0) {
-            latlng = String(latlng).split(',')
-            let lat = parseFloat(latlng[0])
-            let lng = parseFloat(latlng[1])
+            latlng = String(latlng).split(',');
+            let lat = parseFloat(latlng[0]);
+            let lng = parseFloat(latlng[1]);
 
             // TODO: group markers to layers
             let layer = data[row].layer;
 
             // if there is an icon or popup text defined, show a popup icon
-            let iconName = data[row].icon
-            let popupText = data[row].popupText
+            let iconName = data[row].icon;
+            let popupText = data[row].popupText;
 
             let marker = L.marker(L.latLng([lat, lng]));
-            let hasMarker = false
+            let hasMarker = false;
 
             // use custom icon, if iconName is specified
             if (!(iconName === null || iconName.trim() === "") || !(popupText === null || popupText.trim() === "")) {
@@ -214,14 +330,15 @@ function parseCsvMapMarkers(data) {
             // if the coordinates are set to show a home icon, set the home icon
             isHome = String(data[row].home).trim()
             if (isHome.toUpperCase() === "YES" || isHome.toUpperCase() === "TRUE") {
-                homeLng = lng
-                homeLat = lat
+                homeLng = lng;
+                homeLat = lat;
+                newHome = true;
             }
 
         }
     }
 
     // fly to home position
-    map.flyTo([homeLat, homeLng]);
+    if (newHome) map.flyTo([homeLat, homeLng]);
 
 }
